9411147297db9daebeee09385b3d36ee6b985d1f
CaduceusInc <opakunlejo@gmail.com>
2021-10-22 12:42:22 +0100

    install rest and create migrations

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # interna app
+    'apps.endpoints',
 ]
 
 MIDDLEWARE = [
